efe6976b942768a9ddd516925da20eaf4b0c63eb
Daniel Wetzel <mail@danielkbx.com>
2026-04-22 01:58:15 +0200

    fix: switch Homebrew distribution from Formula to Cask
    Formulas trigger a global Xcode version check even for prebuilt
    binaries. Casks skip this check entirely.

---
+++ Casks/ytd.rb
@@ -0,0 +1,18 @@
+cask "ytd" do
+  version "{{VERSION}}"
+
+  on_arm do
+    url "https://github.com/danielkbx/youtrackd/releases/download/v#{version}/ytd-aarch64-apple-darwin.tar.gz"
+    sha256 "{{SHA256_DARWIN_ARM64}}"
+  end
+  on_intel do
+    url "https://github.com/danielkbx/youtrackd/releases/download/v#{version}/ytd-x86_64-apple-darwin.tar.gz"
+    sha256 "{{SHA256_DARWIN_AMD64}}"
+  end
+
+  name "ytd"
+  desc "CLI tool for reading and editing YouTrack tickets and knowledge base articles"
+  homepage "https://github.com/danielkbx/youtrackd"
+
+  binary "ytd"
+end
