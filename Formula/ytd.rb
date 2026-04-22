d37d0b30df7209c82496db44fe51743a5cb03123
Daniel Wetzel <mail@danielkbx.com>
2026-04-22 02:22:40 +0200

    chore: switch Homebrew distribution to source-build formula
    Replaces the prebuilt binary Cask with a Formula that compiles
    from source using Rust. Avoids Gatekeeper issues without requiring
    code signing.

---
+++ Formula/ytd.rb
@@ -0,0 +1,18 @@
+class Ytd < Formula
+  desc "CLI tool for reading and editing YouTrack tickets and knowledge base articles"
+  homepage "https://github.com/danielkbx/youtrackd"
+  url "https://github.com/danielkbx/youtrackd/archive/refs/tags/v{{VERSION}}.tar.gz"
+  sha256 "{{SHA256_SOURCE}}"
+  version "{{VERSION}}"
+  license "MIT"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    assert_match "ytd", shell_output("#{bin}/ytd help")
+  end
+end
